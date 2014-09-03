280580520097d0c7d1139b0d7bac778c763d91a8
thebutterplate <theironhelmet800@gmail.com>
2014-09-02 20:00:04 -0400

    8 ball
    working at it

---
+++ 8 ball.py
@@ -0,0 +1,16 @@
+import random
+
+
+print("say goodbye welcome to da 8 ball")
+
+
+
+answers = ['yes','no','maybe']
+
+while True:
+    print("what is your question")
+    question = input('> ')
+    answer = random.choice(answers)
+    print(answer)
+    print(answer)
+    print()
