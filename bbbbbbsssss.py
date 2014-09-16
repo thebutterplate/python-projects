f70269865f6b3ef317b0ec3e667ce93fd4e1b75a
thebutterplate <theironhelmet800@gmail.com>
2014-09-15 20:17:45 -0400

    BATTEL SHIP
    YYYYYYYYYYYYYYYYYYYYYYYEEEEEEEEEEEEEEEEEEEEEEEEESSSSSSSSSSSSSSSSSSS

---
+++ bbbbbbsssss.py
@@ -0,0 +1,17 @@
+class Ship():
+    size = None
+class Carrier (Ship):
+    size = 5
+class Battleship(Ship):
+    size = 4
+class Submarine(Ship):
+    size = 3
+class Destroyer(ship):
+    size = 3
+class PatrolBoat(Ship):
+    size = 2
+print(Carrier.size)
+print(Battelship.size)
+print(Submarine.size)
+print(Patrolboat.size)
+print(Destroyer.size)
